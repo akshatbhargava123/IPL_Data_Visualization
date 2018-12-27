7960b47e32d49703fc9a566f186aca1b1db725c3
akshat <akshatbhargavadeepa@gmail.com>
2018-12-27 21:38:50 +0530

    added localforage and refactored Home view component to use indexedDb via localforage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
-  <Loading v-if="dataBySeasonsLoading" />
+  <Loading v-if="dataLoading" />
   <SeasonSelect v-else :dataBySeasons="dataBySeasons" @seasonClick="seasonSelected" />
 </template>
 
 <script>
-// @ is an alias to /src
+import localforage from "localforage";
+
 import SeasonSelect from "@/components/SeasonSelect.vue";
 import Loading from "@/components/Loading.vue";
 
 import Match from "@/assets/Match.csv";
+import Ball_by_Ball from "@/assets/Ball_by_Ball.csv";
 
-import store,{ STORE_KEYS } from '../sharedservice';
+import store, { STORE_KEYS } from "../sharedservice";
 
 export default {
   name: "home",
@@ -21,35 +23,69 @@ export default {
   data() {
     return {
       matchesData: null,
-
+      worker: null,
       dataBySeasons: null,
-      dataBySeasonsLoading: true
+      dataLoading: true
     };
   },
   created() {
-    const worker = store.getItem(STORE_KEYS.WORKER);
+    this.worker = store.getItem(STORE_KEYS.WORKER);
+    localforage.setDriver(localforage.INDEXEDDB);
 
-    worker.postMessage('convertToJSON', [Match])
-      .then(result => {
-
-        // Setting Match data for first time
-        store.setItem(STORE_KEYS.MATCH_DATA_JSON, result);
-        this.matchesData = result;
-
-        worker.postMessage('getDataBySeasons', [this.matchesData])
-          .then(res => {
-            this.dataBySeasons = res;
-            this.dataBySeasonsLoading = false;
-            // store.setItem(STORE_KEYS.MATCH_DATA_BY_SEASON, res.map(d => d.matches));
-          });
-      });
+    localforage.getItem("Matches_JSON").then(Matches_JSON => {
+      if (Matches_JSON) {
+        Promise.resolve(Matches_JSON).then(this.MatchJSONReady);
+      } else {
+        this.worker
+          .postMessage("convertToJSON", [Match])
+          .then(this.MatchJSONReady);
+      }
+    });
   },
   methods: {
     seasonSelected(seasonKey) {
       this.$router.push({
-        name: 'season-overview',
+        name: "season-overview",
         params: { season: this.dataBySeasons[seasonKey], id: seasonKey }
       });
+    },
+
+    /**
+     * Callback functions for data loading events
+     */
+    MatchJSONReady(matchesJSON) {
+      localforage.setItem("Matches_JSON", matchesJSON);
+
+      this.matchesData = matchesJSON;
+
+      localforage.getItem("Matches_By_Seasons").then(Matches_By_Seasons => {
+        if (Matches_By_Seasons) {
+          Promise.resolve(Matches_By_Seasons).then(this.MatchesBySeasonsReady);
+        } else {
+          this.worker
+            .postMessage("getDataBySeasons", [this.matchesData])
+            .then(this.MatchesBySeasonsReady);
+        }
+      });
+    },
+    MatchesBySeasonsReady(matchesBySeasons) {
+      localforage.setItem("Matches_By_Seasons", matchesBySeasons);
+      this.dataBySeasons = matchesBySeasons;
+
+      // Load ball data here for later use
+      localforage.getItem("BallByBall_JSON").then(BallbyBallJSON => {
+        if (BallbyBallJSON) {
+          Promise.resolve(BallbyBallJSON).then(this.BallByBallJSONReady);
+        } else {
+          this.worker
+            .postMessage("convertToJSON", [Ball_by_Ball])
+            .then(this.BallByBallJSONReady);
+        }
+      });
+    },
+    BallByBallJSONReady(ballByBallJSON) {
+      localforage.setItem("BallbyBall_JSON", ballByBallJSON);
+      this.dataLoading = false;
     }
   }
 };
