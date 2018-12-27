b3d282f9aa10a069ede2c4f6072eed37e2dd83f8
akshat <akshatbhargavadeepa@gmail.com>
2018-12-27 09:36:16 +0530

    replaced apex-charts with chart.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import VueApexCharts from "vue-apexcharts";
 import VueWorker from "vue-worker";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -11,10 +10,7 @@ import webworkerActions from './webworker.actions';
 import store, { STORE_KEYS } from './sharedservice';
 
 Vue.use(ElementUI);
-Vue.use(VueApexCharts);
 Vue.use(VueWorker);
-
-Vue.component("apexchart", VueApexCharts);
 
 Vue.config.productionTip = false;
 
