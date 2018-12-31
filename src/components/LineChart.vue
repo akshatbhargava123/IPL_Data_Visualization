6002239b7aa98a1402f37551f6c0ea6207214f3f
akshat <akshatbhargavadeepa@gmail.com>
2018-12-31 21:34:56 +0530

    added TeamBasicStats component and finished added runs-outs stat of both teams in MatchOverview component

---
+++ src/components/LineChart.vue
@@ -0,0 +1,32 @@
+<template>
+  <canvas ref="chartCanvas"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js";
+
+export default {
+  props: ["type", "dataProp", "options", "xStepSize", "yStepSize", "showlines"],
+  data() {
+    return {
+      chart: null
+    };
+  },
+  mounted() {
+    this.chart = new Chart(this.$refs.chartCanvas, {
+      data: this.dataProp,
+      options: {
+        ...this.options,
+        showLines: this.showlines
+      },
+      type: this.type
+    });
+  },
+  watch: {
+    dataProp: function() {
+      this.chart.data = this.dataProp;
+      this.chart.update();
+    }
+  }
+}
+</script>
